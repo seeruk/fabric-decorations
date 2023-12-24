1e696cffb21c367bca902fec29e66bdda4d3aa46
Elliot Wright <wright.elliot@gmail.com>
2023-12-24 14:22:14 +0000

    Improved compatibility of Attraction enchantment. Reverted to 1.20.1, we're not going to move past this version just yet actually.

--- src/main/java/dev/seeruk/monsooncraft/mixin/BlockMixin.java
+++ src/main/java/dev/seeruk/monsooncraft/mixin/BlockMixin.java
@@ -1,34 +1,33 @@
 package dev.seeruk.monsooncraft.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.seeruk.monsooncraft.enchant.AttractionEnchantment;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.function.Consumer;
+
 
 @Mixin(Block.class)
 public class BlockMixin {
-    @Inject(
-        method = "afterBreak(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/item/ItemStack;)V",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addExhaustion(F)V", shift = At.Shift.AFTER),
-        cancellable = true
+    @ModifyArg(
+        method = "dropStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)V",
+        at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V")
     )
-    public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity, ItemStack tool, CallbackInfo ci) {
-        if (world instanceof ServerWorld serverWorld && AttractionEnchantment.isHoldingAttractionTool(player)) {
-            Block.getDroppedStacks(state, serverWorld, pos, blockEntity, player, tool).forEach(itemStack -> {
+    private static Consumer<ItemStack> forEachDroppedStack(Consumer<ItemStack> original, @Nullable @Local(argsOnly = true) Entity entity) {
+        return (itemStack -> {
+            if (entity instanceof PlayerEntity player && AttractionEnchantment.isHoldingAttractionTool(player)) {
                 if (player.getInventory().insertStack(itemStack)) {
-                    state.onStacksDropped(serverWorld, pos, tool, true);
-                    ci.cancel();
+                    return;
                 }
-            });
-        }
+            }
+            original.accept(itemStack);
+        });
     }
 }
